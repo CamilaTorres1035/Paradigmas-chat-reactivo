b50e3f6ac988db93c8677237a2de9afb2ecbdfcd
CamilaTorres5 <camila.torres5@udea.edu.co>
2025-09-20 14:19:19 -0500

    chore(firebase): agregar configuración inicial en app.config.ts
    - Se importan módulos de Firebase (app, firestore, database)
    - Se añade objeto firebaseConfig con credenciales del proyecto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,20 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+// Firebase imports
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+
+// Web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_XJlWFdZvj8neSKh8u291WqcWcNfQeyg",
+  authDomain: "chat-reactivo.firebaseapp.com",
+  projectId: "chat-reactivo",
+  storageBucket: "chat-reactivo.firebasestorage.app",
+  messagingSenderId: "600409525329",
+  appId: "1:600409525329:web:9b0bdab90a36d416d63c70"
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
